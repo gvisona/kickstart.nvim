2b65fd8dcaf02c68a4cae128c1c4ac3fc11e4eb9
gvisona <giovanni@gvisona.com>
2025-03-12 17:34:24 +0100

    added copilot options

--- lua/custom/plugins/copilotchat.lua
+++ lua/custom/plugins/copilotchat.lua
@@ -4,7 +4,33 @@ return {
     cmd = 'Copilot',
     event = 'InsertEnter',
     config = function()
-      require('copilot').setup {}
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = false,
+          hide_during_completion = true,
+          debounce = 75,
+          keymap = {
+            accept = '<M-y>',
+            accept_word = false,
+            accept_line = false,
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ['.'] = false,
+        },
+      }
     end,
   },
   {
