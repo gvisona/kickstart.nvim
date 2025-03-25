d57099cb33e44dee73948fd42bfcc3cf1c138d8b
gvisona <giovanni@gvisona.com>
2025-03-25 08:37:55 +0100

    removed col limitation in markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,5 +2,5 @@ vim.opt_local.conceallevel = 2
 vim.opt_local.wrap = true -- Wrap text
 vim.opt_local.breakindent = true -- Match indent on line break
 vim.opt_local.linebreak = true
-vim.opt_local.columns = 140
+-- vim.opt_local.columns = 140
 vim.opt_local.colorcolumn = ''
